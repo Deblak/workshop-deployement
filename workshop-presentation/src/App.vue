9f92c9096838ec119712200560c8affcf8f412e8
Deblak <deblak@mailo.com>
2025-07-08 23:33:26 +0200

    init and install presentation

---
+++ workshop-presentation/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <small>Workshop Jenkins</small>
+  </header>
+  
+  <main>
+    <h1>My first deployement... and Vue.js</h1>
+    <p>Customize as you wish</p>
+  </main>
+</template>
