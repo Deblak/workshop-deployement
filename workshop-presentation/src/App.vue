5e618811affc48f25cdc0374da70a48d709037d3
Deblak <deblak@mailo.com>
2025-07-09 21:41:51 +0200

    add API fetch

--- workshop-presentation/src/App.vue
+++ workshop-presentation/src/App.vue
@@ -1,10 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const projects = ref([])
+
+onMounted(async () => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/projects/all`)
+  projects.value = await response.json()
+})
+</script>
+
 <template>
   <header>
-    <small>Workshop Jenkins</small>
+    <small>Workshop Jenkins - customize as you wish</small>
   </header>
-  
+
   <main>
     <h1>My first deployement... and Vue.js</h1>
-    <p>Customize as you wish</p>
+    <section>
+      <h2>Projects reminder:</h2>
+      <ul>
+        <li v-for="name in projects" :key="name">
+          {{ name }}
+        </li>
+      </ul>
+      <small>This is not exhaustive</small>
+    </section>
   </main>
 </template>
